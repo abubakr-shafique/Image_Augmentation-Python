ee34894acd86db27c5a0321db1e8113a18d47f36
Abubakr Shafique <54443371+abubakr-shafique@users.noreply.github.com>
2020-01-05 20:52:25 +0800

    Image Rotation

--- Image_Rotation.py
+++ Image_Rotation.py
@@ -1,53 +1,36 @@
 ## This Program is Written by Abubakr Shafique (abubakr.shafique@gmail.com)
 
-import numpy as np
-import cv2 as cv 
+from PIL import Image 
 
+def Rotate_Clockwise(Input_Image):
 
-def Rotate_Clockwise(Image):
-    Height, Width, Channels = Image.shape
+    New_Image = Input_Image.transpose(Image.ROTATE_270)
     
-    New_Image = np.zeros(Image.shape, dtype=np.uint8)
-    
-    for i in range(Height):
-        for j in range(Width):
-            New_Image[i, j] = Image[Height-j-1, Width-i-1]
-            New_Image = New_Image[0:Height, 0:Width]
     return New_Image
 
-def Rotate_Counter_Clockwise(Image):
-    Height, Width, Channels = Image.shape
+def Rotate_Counter_Clockwise(Input_Image):
+
+    New_Image = Input_Image.transpose(Image.ROTATE_90)
     
-    New_Image = np.zeros(Image.shape, dtype=np.uint8)
-    
-    for i in range(Height):
-        for j in range(Width):
-            New_Image[i, j] = Image[j-1, i-1]
-            New_Image = New_Image[0:Height, 0:Width]
     return New_Image
 
-def Rotate_Upside_Down(Image):
-    Height, Width, Channels = Image.shape
+def Rotate_Upside_Down(Input_Image):
+
+    New_Image = Input_Image.transpose(Image.ROTATE_180)
     
-    New_Image = np.zeros(Image.shape, dtype=np.uint8)
-    
-    for i in range(Height):
-        for j in range(Width):
-            New_Image[i, j] = Image[Height-i-1, Width-j-1]
-            New_Image = New_Image[0:Height, 0:Width]
     return New_Image
 
 def main():
-    Input_Image = cv.imread('Test_Image.png')
+    Input_Image = Image.open('Test_Image3.png')
+
+    CW_Rotated_Image = Rotate_Clockwise(Input_Image) 
+    CW_Rotated_Image.save('CW_Rotated_Image.png')
+
+    CCW_Rotated_Image = Rotate_Counter_Clockwise(Input_Image) 
+    CCW_Rotated_Image.save('CCW_Rotated_Image.png')
     
-    Image_CW = Rotate_Clockwise(Input_Image)
-    cv.imwrite('Image_CW.png', Image_CW)
-    
-    Image_CCW = Rotate_Counter_Clockwise(Input_Image)
-    cv.imwrite('Image_CCW.png', Image_CCW)
-    
-    Image_UD = Rotate_Upside_Down(Input_Image)
-    cv.imwrite('Image_Upside_Down.png', Image_UD)
+    UD_Rotated_Image = Rotate_Upside_Down(Input_Image) 
+    UD_Rotated_Image.save('UD_Rotated_Image.png')
     
     input('Please Enter to Continue...')
 
